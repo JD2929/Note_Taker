bfd2131ee900ccd5f42dcf37d0c3d68fc870539f
JD2929 <jandunk@creonova.com>
2023-09-12 16:40:06 -0300

    Added POST route to enable the creation of new notes.

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const fs = require ('fs');
-
+const uniqid = require('uniqid');
 
 //goes along with the fetch in index.js on line 29
 router.get('/api/notes', (req, res) =>{
@@ -12,8 +12,14 @@ router.get('/api/notes', (req, res) =>{
 
 //goes along with the fetch in index.js on line 37
 
-//app.post
-
+router.post('/api/notes', (req, res) => {
+    const noteDb = JSON.parse(fs.readFileSync("db/db.json"))
+    const newNote = req.body
+    newNote.id = uniqid()
+    noteDb.push(newNote)
+    fs.writeFileSync('db/db.json', JSON.stringify(noteDb))
+    res.json(noteDb)
+});
 
 //goes along with the fetch in index.js on line 46
 
