d9c086b3dffbb4f5528882fd295c63fe00e914c4
JD2929 <jandunk@creonova.com>
2023-09-12 16:47:37 -0300

    Coded in the delete note function.

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const fs = require ('fs');
 const uniqid = require('uniqid');
 
-//goes along with the fetch in index.js on line 29
+
 router.get('/api/notes', (req, res) =>{
     fs.readFile("db/db.json", "utf-8", (err,data) => {
         if(err) throw err
@@ -10,7 +10,7 @@ router.get('/api/notes', (req, res) =>{
     })
 });
 
-//goes along with the fetch in index.js on line 37
+
 
 router.post('/api/notes', (req, res) => {
     const noteDb = JSON.parse(fs.readFileSync("db/db.json"))
@@ -21,8 +21,12 @@ router.post('/api/notes', (req, res) => {
     res.json(noteDb)
 });
 
-//goes along with the fetch in index.js on line 46
 
-//app.delete
 
+router.delete('/api/notes/:id', (req, res) => {
+    const noteDb = JSON.parse(fs.readFileSync("db/db.json")) 
+    const deleteNote = noteDb.filter ((removeNote) => removeNote.id !==req.params.id) 
+    fs.writeFileSync('db/db.json', JSON.stringify(deleteNote))
+    res.json(deleteNote)
+})
 module.exports = router;
